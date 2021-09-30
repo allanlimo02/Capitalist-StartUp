3629b1112a01d8da45b98c3202ae5d98cd40f3be
lanarokip <lankipkoecharo@gmail.com>
2021-09-30 23:51:49 +0300

    added the taste files for the dao clases

--- src/test/java/sql2oDao/Sql2oCapitalistsTest.java
+++ src/test/java/sql2oDao/Sql2oCapitalistsTest.java
@@ -1,11 +1,14 @@
 package sql2oDao;
 
+import model.Capitalists;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class Sql2oCapitalistsTest {
+        Sql2oCapitalists capitalistsdao= new Sql2oCapitalists();
 
     @Before
     public void setUp() throws Exception {
@@ -13,5 +16,30 @@ public class Sql2oCapitalistsTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void create() {
+        Capitalists capitalists = new Capitalists("lion",7,"fight");
+        int myid = capitalists.getId();
+        capitalistsdao.create(capitalists);
+        assertNotEquals(myid,capitalists.getId());
+    }
+
+    @Test
+    public void findById() {
+        Capitalists capitalists = new Capitalists("rashid",3,"biz");
+        capitalistsdao.create(capitalists);
+        Capitalists foundCapitalist = capitalistsdao.findById(capitalists.getId());
+        assertNotEquals(capitalists,foundCapitalist);
+    }
+
+    @Test
+    public void deleteAll() {
+    }
+
+    @Test
+    public void deleteById() {
     }
 }

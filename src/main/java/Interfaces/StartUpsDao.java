651e0567609c61c83d01f2f7f25407aa87f5eee1
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-30 22:00:09 +0300

    creates all the interfaces needed for the databases access

--- src/main/java/Interfaces/StartUpsDao.java
+++ src/main/java/Interfaces/StartUpsDao.java
@@ -1,4 +1,21 @@
 package Interfaces;
 
+import java.StartUps;
+import java.util.List;
+
 public interface StartUpsDao {
+    //create
+    void add(StartUps startUps);
+
+    //read
+    List<StartUps> getAll();
+    StartUps findById(int id);
+
+    //update
+    void update(String newStartUp, int id);
+
+    //delete
+    void deleteAll();
+    void deleteById(int id);
+
 }

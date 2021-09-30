28a1f47442d874cdefca2e9998df568fca791666
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-10-01 00:01:49 +0300

    creates test files for the startUp class

--- src/test/java/StartUpsTest.java
+++ src/test/java/StartUpsTest.java
@@ -1,11 +1,14 @@
+//package ;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.StartUps;
+
 import static org.junit.Assert.*;
 
 public class StartUpsTest {
-    StartUps startUps;
 
     @Before
     public void setUp() throws Exception {
@@ -17,7 +20,36 @@ public class StartUpsTest {
 
     @Test
     public void startUpInstantiatesCorrectly() throws Exception{
-        StartUps testStartUp = new StartUps("Jeff Bezos", "Amazon", "E-commerce", 500);
+        StartUps testStartUp = setUpStartUp();
         assertEquals(true, testStartUp instanceof StartUps);
+    }
+
+    @Test
+    public void startUpInstantiatesWithName() throws  Exception{
+        StartUps testStartUp = setUpStartUp();
+        assertEquals("Jeff Bezos", testStartUp.getName());
+    }
+
+    @Test
+    public void startUpInstantiatesWithStartUpName() throws  Exception{
+        StartUps testStartUp = setUpStartUp();
+        assertEquals("Amazon", testStartUp.getName_of_startup());
+    }
+
+    @Test
+    public void startUpInstantiatesWithCategory(){
+        StartUps testStartUp = setUpStartUp();
+        assertEquals("E-commerce", testStartUp.getCategory());
+    }
+
+    @Test
+    public void startUpInstantiatesWithCapital(){
+        StartUps testStartUp = setUpStartUp();
+        assertEquals(500, testStartUp.getCapital_Needed())
+    }
+
+    // Helper methods
+    public StartUps setUpStartUp(){
+        return new StartUps("Jeff Bezos", "Amazon", "E-commerce", 500);
     }
 }
